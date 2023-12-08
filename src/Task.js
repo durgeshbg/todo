d3f3b0d84b0c16ab67592f4f8f5852bde1d72091
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-08 17:53:46 +0530

    add: Task factory function

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,14 @@
 function generateUID() {
     return Date.now().toString(32) + Math.random().toString(32).slice(2);
 }
-
+function Task(title, notes, duedate, priority) {
+    return {
+        uid: generateUID(),
+        title,
+        notes,
+        duedate,
+        priority,
+        complete: false,
+    };
+}
+export { Task };
