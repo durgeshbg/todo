7cebba7ceb25417db5041de6bcfe5b1d3ccc513d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-09 22:25:21 +0530

    add: addEventListener for .add
    to display form when clicked

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Testing');
+import { init, getStore, setStore } from './store';
+import { Task, Project, addTask, deleteTask, getTask } from './Task';
+import { getDOM, displayForm } from './DOM';
+
+init();
+getDOM('.add-project').addEventListener('click', displayForm);
+getDOM('.add-task').addEventListener('click', displayForm);
