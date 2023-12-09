aeeb13e290486a4e01bed76fb17f1ca23b5633c8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-09 22:38:39 +0530

    refactor: use much short syntax for event listener

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import { Task, Project, addTask, deleteTask, getTask } from './Task';
 import { getDOM, displayForm, closeForm } from './DOM';
 
 init();
-getDOM('.add-project').addEventListener('click', displayForm);
-getDOM('.add-task').addEventListener('click', displayForm);
-getDOM('.task-form button[type="reset"]').addEventListener('click', closeForm);
-getDOM('.project-form button[type="reset"]').addEventListener(
-    'click',
-    closeForm
-);
+getDOM('.add-project').onclick = displayForm;
+getDOM('.add-task').onclick = displayForm;
+getDOM('.task-form button[type="reset"]').onclick = closeForm;
+getDOM('.project-form button[type="reset"]').onclick = closeForm;
