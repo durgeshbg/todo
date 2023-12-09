4457c76ff85f85b85ed6ada01031479ffe09f993
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-09 22:34:01 +0530

    add: event listener for reset button
    to close the Form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { init, getStore, setStore } from './store';
 import { Task, Project, addTask, deleteTask, getTask } from './Task';
-import { getDOM, displayForm } from './DOM';
+import { getDOM, displayForm, closeForm } from './DOM';
 
 init();
 getDOM('.add-project').addEventListener('click', displayForm);
 getDOM('.add-task').addEventListener('click', displayForm);
+getDOM('.task-form button[type="reset"]').addEventListener('click', closeForm);
+getDOM('.project-form button[type="reset"]').addEventListener(
+    'click',
+    closeForm
+);
