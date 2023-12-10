e682d34f0a0e4c9e17a50c0a1865f565eb3ecef0
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-10 22:05:18 +0530

    add: writeTasksBar function
    to fetch all tasks when relevant project is clicked

--- src/domWrite.js
+++ src/domWrite.js
@@ -1,14 +1,27 @@
 import { getDOM } from './domEvents';
-import { getStore } from './store';
+import { getProject, getStore } from './store';
+import { writeTask } from './taskTemplate';
 
 export function writeProjectsBar() {
     const projectsBar = getDOM('.projects');
     let projects = getStore();
     for (let uid in projects) {
         let project = projects[uid];
-        let projectContainer = document.createElement('div');
-        projectContainer.classList.add(uid, 'project');
-        projectContainer.innerHTML = `${project.name} <div class="trash">x</div>`;
-        projectsBar.appendChild(projectContainer);
+        let projectdiv = document.createElement('div');
+        projectdiv.classList.add(uid, 'project');
+        projectdiv.innerHTML = `${project.name} <div class="trash">x</div>`;
+        projectdiv.onclick = writeTasksBar;
+        projectsBar.appendChild(projectdiv);
     }
+}
+
+export function writeTasksBar(e) {
+    let uid = e.target.classList[0];
+    let tasks = getProject(uid).list;
+    let taskBar = getDOM('.tasks');
+    taskBar.innerHTML = '';
+    tasks.forEach((task) => {
+        let taskdiv = writeTask(task);
+        taskBar.appendChild(taskdiv);
+    });
 }
