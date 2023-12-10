bfccf43017e4b5b31c148d147f1610f61dfc5722
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-10 13:28:50 +0530

    refactor: change forms to dialogs

--- src/DOM.js
+++ src/DOM.js
@@ -2,17 +2,17 @@ export function getDOM(q) {
     return document.querySelector(q);
 }
 export function displayForm(e) {
+    let dialog;
     let category = e.target.classList[1];
-    let form;
-    if (category == 'add-task') {
-        form = getDOM('.task-form');
-    } else {
-        form = getDOM('.project-form');
-    }
-    form.style.display = '';
+    category = category.split('-')[1];
+    dialog = getDOM(`#${category}-dialog`);
+    dialog.show();
 }
 
 export function closeForm(e) {
-    let div = e.target.parentElement;
-    div.parentElement.style.display = 'none';
+    let dialog;
+    let category = e.target.classList[0];
+    category = category.split('-')[0];
+    dialog = getDOM(`#${category}-dialog`);
+    dialog.close();
 }
