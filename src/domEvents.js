28ac7cb01346a628fd891caaf9f3c0ac0ac7958c
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-11 12:16:21 +0530

    add: FormEvents function
    to handle all form control events

---
+++ src/domEvents.js
@@ -0,0 +1,17 @@
+import {
+    getDOM,
+    displayForm,
+    closeForm,
+    handleSubmit,
+} from './domControllers';
+
+export function FormEvents() {
+    getDOM('.add-project').onclick = displayForm;
+    getDOM('.add-task').onclick = displayForm;
+    getDOM('.task-form button[type="reset"]').onclick = closeForm;
+    getDOM('.project-form button[type="reset"]').onclick = closeForm;
+    getDOM('.task-form').onsubmit = handleSubmit;
+    getDOM('.project-form').onsubmit = handleSubmit;
+}
+
+
