f45af237d417c376644ab9c96af790947946a286
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-12 01:07:32 +0530

    add: initTaskBarEvents
    handles all TaskBar events

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import {
     FormEvents as initFormEvents,
     ProjectBarEvents as initProjectBarEvents,
+    TaskBarEvents as initTaskBarEvents,
 } from './domEvents';
 import { init as initStore } from './store';
 
 initStore();
 initFormEvents();
 initProjectBarEvents();
+initTaskBarEvents();
