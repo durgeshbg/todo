0c6d1430d1a819684496d5057f271340c5e9321e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-11 12:24:46 +0530

    update: import FormEvents from domEvents module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import { init, getStore, setStore } from './store';
-import { Task, Project, addTask, deleteTask, getTask } from './Task';
-import {
-    getDOM,
-    displayForm,
-    closeForm,
-    handleSubmit,
-    getallDOM,
-} from './domEvents';
-import { writeProjectsBar, writeTasksBar } from './domWrite';
+import { FormEvents as initFormEvents } from './domEvents';
+import { init as initStore } from './store';
 
-init();
-getDOM('.add-project').onclick = displayForm;
-getDOM('.add-task').onclick = displayForm;
-getDOM('.task-form button[type="reset"]').onclick = closeForm;
-getDOM('.project-form button[type="reset"]').onclick = closeForm;
-getDOM('.task-form').onsubmit = handleSubmit;
-getDOM('.project-form').onsubmit = handleSubmit;
-getallDOM('.project').forEach((project) => {
-    project.onclick = writeTasksBar;
-});
-writeProjectsBar();
+initStore();
+initFormEvents();
