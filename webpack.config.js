f01ed778a45212a7f2b67eed48c22a215997babf
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-18 08:33:30 +0530

    add: html-webpack-plugin plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    devtool: 'inline-source-map',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/templates/index.html',
+    }),
+  ],
 };
