ccef12cd69eee66eb3465b968275b3473780a405
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-18 09:23:03 +0530

    add: image loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
